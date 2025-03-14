1f31611a085620d7b3357e4a16a8cdd2dbdefa7c
RaduTek <me@radutek.uk>
2025-03-14 20:41:25 +0200

    Enhance MUI theme with custom typography settings

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,5 +1,10 @@
-import { createTheme } from "@mui/material";
+"use client";
+import { createTheme } from "@mui/material/styles";
 
-const theme = createTheme({});
+const theme = createTheme({
+    typography: {
+        fontFamily: "var(--font-roboto)",
+    },
+});
 
 export default theme;
