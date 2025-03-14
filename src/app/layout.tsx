ea19700ed80d0c84ee8d00d9adbef52c5f517950
RaduTek <me@radutek.uk>
2025-03-14 20:54:53 +0200

    Fix font usage for nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import theme from "./theme";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import { Roboto } from "next/font/google";
+
+import theme from "../theme";
+import Navbar from "@/components/layout/Navbar";
 
 export const metadata: Metadata = {
     title: { default: "TeamUp", template: "%s | TeamUp" },
     description: "Find people to collaborate with",
 };
+
+const roboto = Roboto({
+    weight: ["300", "400", "500", "700"],
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-roboto",
+});
 
 export default function RootLayout({
     children,
@@ -21,10 +27,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body cz-shortcut-listen="true">
+            <body cz-shortcut-listen="true" className={roboto.variable}>
                 <ClerkProvider>
                     <AppRouterCacheProvider>
-                        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+                        <ThemeProvider theme={theme}>
+                            <Navbar />
+                            <CssBaseline />
+                            {children}
+                        </ThemeProvider>
                     </AppRouterCacheProvider>
                 </ClerkProvider>
             </body>
