34ee6bdf87181f75a5f9c09e58505776da8cb238
AndyStan112 <stanica.andrei28@gmail.com>
2025-03-16 10:39:07 +0200

    set profile pic to default when none selected instead of empty

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+export const urlToFile = async (url: string) => {
+    console.log(url);
+    const response = await fetch(url);
+
+    const blob = await response.blob();
+    const t = await response.body;
+    console.log(t);
+    console.log(blob);
+    const file = new File([blob], "image.jpg", { type: blob.type });
+    console.log(file);
+    return file;
+};
