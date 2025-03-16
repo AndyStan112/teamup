b53ab8298af9e35497ab27baf0917a6453eadd10
AndyStan112 <stanica.andrei28@gmail.com>
2025-03-16 15:02:05 +0200

    purge db

--- prisma/migrations/20250315161037_unique_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[swiperId,swipedId]` on the table `SwipeUser` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "SwipeUser_swiperId_swipedId_key" ON "SwipeUser"("swiperId", "swipedId");
--- prisma/migrations/20250316045814_add_chat_data/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Chat" ADD COLUMN     "imageUrl" TEXT DEFAULT '',
-ADD COLUMN     "name" TEXT NOT NULL DEFAULT '';
