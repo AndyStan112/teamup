027098b5fd27916eeb343050127e8814bd32d380
AndyStan112 <stanica.andrei28@gmail.com>
2025-03-16 12:57:16 +0200

    redirect to profile if there is no user in db but you are authed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,34 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isPublicRoute = createRouteMatcher(['/'])
-
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { prisma } from "./utils";
+import { NextResponse } from "next/server";
+import { auth as a } from "@clerk/nextjs/server";
+const isPublicRoute = createRouteMatcher(["/"]);
 export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
-  }
-})
+    if (!isPublicRoute(request)) {
+        const { userId } = await a();
+
+        if (!userId) {
+            await auth.protect();
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId! },
+            select: { id: true },
+        });
+
+        if (!user) {
+            return NextResponse.redirect(new URL("/profile", request.url));
+        }
+    }
+
+    return NextResponse.next();
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        // Always run for API routes
+        "/(api|trpc)(.*)",
+    ],
+};
