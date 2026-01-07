4695c79ef06901df0a43a2578fa3db4fca9c1b9b
AndyStan112 <stanica.andrei28@gmail.com>
2026-01-07 13:06:10 +0200

    add roles

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export const prisma = new PrismaClient();
 export const urlToFile = async (url: string) => {
     console.log(url);
     const response = await fetch(url);
-
+    
     const blob = await response.blob();
     const t = await response.body;
     console.log(t);
@@ -13,3 +13,11 @@ export const urlToFile = async (url: string) => {
     console.log(file);
     return file;
 };
+//TODO
+//example, make yout own util file cuz this one is used in client components too and throws an error. Can make other methods too etc @radutek, @enilesi
+// import { Roles } from '@/types/globals'
+// import { auth } from '@clerk/nextjs/server'
+// export const checkRole = async (role: Roles) => {
+//   const { sessionClaims } = await auth()
+//   return sessionClaims?.metadata.role === role
+// }
