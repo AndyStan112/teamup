ee1a6f6cb862da4dae4c1af8e6667cec189cecb3
Enilesi <miriam.iseline@gmail.com>
2026-01-11 19:40:13 +0200

    fixed generate ideas background

--- src/app/generateIdeas/layout.tsx
+++ src/app/generateIdeas/layout.tsx
@@ -1,28 +1,24 @@
-"use client";
-
-import { Box, Container, Paper, useTheme} from "@mui/material";
+import { Box, Container, Paper } from "@mui/material";
 import React from "react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const theme = useTheme();
-
     return (
         <Box minHeight="80vh" position="relative">
             <Box
-            sx={{
-                minHeight: "calc(100vh - 64px)",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                background: `linear-gradient(90deg, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
-            }}
-        >
+                height="45vh"
+                width="100%"
+                position="absolute"
+                top={0}
+                zIndex={-500}
+                sx={{
+                    backgroundImage: "linear-gradient(to right, #ca5a29, #a31755)",
+                }}
+            ></Box>
             <Container maxWidth="md" sx={{ py: 3 }}>
                 <Paper elevation={5} sx={{ borderRadius: "32px" }}>
                     {children}
                 </Paper>
             </Container>
-            </Box>
         </Box>
     );
 };
--- src/app/generateIdeas/page.tsx
+++ src/app/generateIdeas/page.tsx
@@ -71,8 +71,7 @@ export default function Page() {
     };
 
     return (
-        
-        <Stack gap={2} p={3} >
+        <Stack gap={2} p={3}>
             <Typography variant="h5" align="center">
                 Generate Ideas
             </Typography>
